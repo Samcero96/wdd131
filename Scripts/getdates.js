9ebab921a4066c79deff85a914d722baef78918b
Samcero96 <dayoayanleke96@gmail.com>
2025-09-24 12:40:00 +0100

    updated

--- Scripts/getdates.js
+++ Scripts/getdates.js
@@ -1,8 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
-    // Insert the current year into the <span>
+
     document.getElementById("currentyear").textContent = new Date().getFullYear();
 
-    // Insert the page's last-modified date into the second <p>
     document.getElementById("lastModified").textContent =
         "Last Modification: " + document.lastModified;
 });
